0b34bc8fadc9f7d7af49f7cad3c7ca5038eb7a24
[O] iMPDevMC <47728875+iDevMC@users.noreply.github.com>
2019-02-18 18:02:19 -0500

    Create Data.java

---
+++ gq/devmc/zmaint/utilities/Data.java
@@ -0,0 +1,13 @@
+package gq.devmc.zmaint.utilities;
+
+import java.util.List;
+
+public class Data
+{
+  public static String prefix;
+  public static String servername;
+  public static boolean maintenance;
+  public static String maintenancereason;
+  public static String maintenanceduration;
+  public static List<String> whitelist;
+}
